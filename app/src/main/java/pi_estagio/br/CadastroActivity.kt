8112e5afb3f2058a5439ef859857aaafb6c32c1e
Lamarcaa <g.lamarca@outlook.com>
2023-04-22 11:43:22 -0300

    primeiro commit

---
+++ app/src/main/java/pi_estagio/br/CadastroActivity.kt
@@ -0,0 +1,23 @@
+package pi_estagio.br
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle??) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+
+                // get reference to the string array that we just created
+                val languages = resources.getStringArray(R.array.opcoes_perfil)
+                // create an array adapter and pass the required parameter
+                // in our case pass the context, drop down layout , and array.
+                val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, languages)
+                // get reference to the autocomplete text view
+                val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+                // set adapter to the autocomplete tv to the arrayAdapter
+                autocompleteTV.setAdapter(arrayAdapter)
+            }
+        }
---
+++ app/src/main/java/pi_estagio/br/MainActivity.kt
@@ -0,0 +1,62 @@
+package pi_estagio.br
+
+import pi_estagio.br.CadastroActivity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.content.Intent
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val emailLogar = findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.login_email)
+        val senhaLogar = findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.login_senha)
+        val bntLogar = findViewById<MaterialButton>(R.id.btn_logar)
+        val btnCadastrar = findViewById<MaterialButton>(R.id.btn_cadastrar)
+
+        bntLogar.setOnClickListener {
+
+            /* se n colocar o toString n pega a string digitada */
+            val email = emailLogar.editText?.text.toString()
+            val senha = senhaLogar.editText?.text.toString()
+
+
+            /* se o login_email e login_senha estiver vazios n deixa passar */
+            if (email.isEmpty() || senha.isEmpty()) {
+
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+
+            } else {
+
+                if (email == "teste@gmail.com" && senha == "123456") {
+
+                    /* intent para entrar na home caso os dados estiverem corretos */
+                    bntLogar.setOnClickListener {
+                        val intentLogar = Intent(this, home::class.java)
+                        startActivity(intentLogar)
+                    }
+
+                } else {
+
+                    Toast.makeText(this, "Email ou senha incorretos", Toast.LENGTH_SHORT).show()
+                }
+
+
+            }
+        }
+
+        /* intent para ir para o cadastro */
+        btnCadastrar.setOnClickListener {
+            val intentCadastro = Intent(this, CadastroActivity::class.java)
+            startActivity(intentCadastro)
+        }
+    }
+}
