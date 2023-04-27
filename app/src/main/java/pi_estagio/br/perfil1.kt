2ac188e65229e67f21373a297ba903b8922d2fcb
rafaelvaz00 <rafael00vaz@outlook.com>
2023-04-27 20:55:11 -0300

    duas telas de perfil adicionadas

---
+++ app/src/main/java/pi_estagio/br/perfil1.kt
@@ -0,0 +1,41 @@
+package pi_estagio.br
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class perfil1 : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_perfil1, container, false);
+
+
+        var botaoEditar=view.findViewById<com.google.android.material.button.MaterialButton>(R.id.btn_editarPerfil);
+        val botaoSair=view.findViewById<com.google.android.material.button.MaterialButton>(R.id.btn_sairApp);
+
+        botaoEditar.setOnClickListener{
+            val intentLogar = Intent(this.context, Inicio::class.java)
+            startActivity(intentLogar)
+
+        }
+
+        botaoSair=
+        // Inflate the layout for this fragment
+        return view;
+
+    }
+
+
+}
