33ae496b4089a35b5ca10e7d8546d98895d8bc89
rafaelvaz00 <rafael00vaz@outlook.com>
2023-04-28 21:05:51 -0300

    Lógica de cadastro e de login implementadas. Campo de tipo de conta removido temporariamente.

--- app/src/main/java/pi_estagio/br/Cadastro.kt
+++ app/src/main/java/pi_estagio/br/Cadastro.kt
@@ -1,23 +1,57 @@
 package pi_estagio.br
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class Cadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle??) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
 
-                // get reference to the string array that we just created
-                val languages = resources.getStringArray(R.array.opcoes_perfil)
-                // create an array adapter and pass the required parameter
-                // in our case pass the context, drop down layout , and array.
-                val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, languages)
-                // get reference to the autocomplete text view
-                val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
-                // set adapter to the autocomplete tv to the arrayAdapter
-                autocompleteTV.setAdapter(arrayAdapter)
+        val cad_nome_login =
+            findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.cad_nome_login)
+        val cad_email_login =
+            findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.cad_email_login)
+        val cad_senha_login =
+            findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.cad_senha_login)
+        val cad_conf_senha =
+            findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.cad_conf_senha)
+
+        val bntCadastrar = findViewById<MaterialButton>(R.id.btn_logar)
+        val bntVoltar = findViewById<MaterialButton>(R.id.btn_voltar)
+
+        bntCadastrar.setOnClickListener {
+            val nome = cad_nome_login.editText?.text.toString()
+            val email = cad_email_login.editText?.text.toString()
+            val senha = cad_senha_login.editText?.text.toString()
+            val confSenha = cad_conf_senha.editText?.text.toString()
+
+            /* verificação dos campos */
+            if (nome.isEmpty() || email.isEmpty() || senha.isEmpty() || confSenha.isEmpty()) {
+
+                Toast.makeText(this, "Campos nao preenchidos", Toast.LENGTH_SHORT).show()
+
+            } else {
+
+                if (senha == confSenha) {
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("email", email)
+                    intent.putExtra("senha", senha)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(this, "Senhas incorretas", Toast.LENGTH_SHORT).show()
+                }
             }
         }
+
+        bntVoltar.setOnClickListener {
+            val intentVoltarLogin = Intent(this, MainActivity::class.java)
+            startActivity(intentVoltarLogin)
+        }
+    }
+}
--- app/src/main/java/pi_estagio/br/MainActivity.kt
+++ app/src/main/java/pi_estagio/br/MainActivity.kt
@@ -1,10 +1,10 @@
 package pi_estagio.br
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import android.content.Intent
 import com.google.android.material.button.MaterialButton
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,52 +12,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val emailLogar =
-            findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.login_email)
-        val senhaLogar =
-            findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.login_senha)
+        val emailLogar = findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.login_email)
+        val senhaLogar = findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.login_senha)
         val bntLogar = findViewById<MaterialButton>(R.id.btn_logar)
         val btnCadastrar = findViewById<MaterialButton>(R.id.btn_cadastrar)
 
         bntLogar.setOnClickListener {
 
-            /* se n colocar o toString n pega a string digitada */
-            val email = emailLogar.editText?.text.toString()
-            val senha = senhaLogar.editText?.text.toString()
+            /* precisa pegar a string que esta sendo digitada pra ver se esta vazio */
+            val emailLogar1 = emailLogar.editText?.text.toString()
+            val senhaLogar1 = senhaLogar.editText?.text.toString()
 
 
             /* se o login_email e login_senha estiver vazios n deixa passar */
-            if (email.isEmpty() || senha.isEmpty()) {
+            if (emailLogar1.isEmpty() || senhaLogar1.isEmpty()) {
 
                 Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
 
             } else {
 
-                if (email == "teste@g" && senha == "123") {
+                /* variaveis recebidas do cadastro */
+                val emailCadastrado = intent.getStringExtra("email")
+                val senhaCadastrada = intent.getStringExtra("senha")
 
-                    /* intent para entrar na home caso os dados estiverem corretos */
+                if (emailLogar1 == emailCadastrado && senhaLogar1 == senhaCadastrada) {
+
                     bntLogar.setOnClickListener {
-                        val intentLogar = Intent(this, TelaInicial::class.java)
-                        startActivity(intentLogar)
+                        val intentVagas = Intent(this, TelaInicial::class.java)
+                        startActivity(intentVagas)
                     }
 
                 } else {
 
                     Toast.makeText(this, "Email ou senha incorretos", Toast.LENGTH_SHORT).show()
                 }
-
-
             }
         }
 
         /* intent para ir para o cadastro */
-        btnCadastrar.setOnClickListener {
-            val intentCadastro = Intent(this, Cadastro::class.java)
-            startActivity(intentCadastro)
+        btnCadastrar.setOnClickListener{
+            val intentCadastrar = Intent(this, Cadastro::class.java)
+            startActivity(intentCadastrar)
         }
 
-
     }
-
 }
