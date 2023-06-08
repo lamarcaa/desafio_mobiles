0c44671aec1519ab140666de0e79fcc7b757be7a
rafaelvaz00 <rafael00vaz@outlook.com>
2023-06-08 19:43:35 -0300

    Login com firebase adicionado

--- app/src/main/java/pi_estagio/br/MainActivity.kt
+++ app/src/main/java/pi_estagio/br/MainActivity.kt
@@ -5,56 +5,50 @@ import android.os.Bundle
 import android.widget.Toast
 import android.content.Intent
 import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
+import pi_estagio.br.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val emailLogar = findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.login_email)
-        val senhaLogar = findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.login_senha)
-        val bntLogar = findViewById<MaterialButton>(R.id.btn_logar)
-        val btnCadastrar = findViewById<MaterialButton>(R.id.btn_cadastrar)
+        binding.btnLogar.setOnClickListener {
 
-        bntLogar.setOnClickListener {
+            if (binding.loginEmail.text.toString().isEmpty()) {
+                Toast.makeText(this, "Insira um Email!", Toast.LENGTH_SHORT).show()
 
-            /* precisa pegar a string que esta sendo digitada pra ver se esta vazio */
-            val emailLogar1 = emailLogar.editText?.text.toString()
-            val senhaLogar1 = senhaLogar.editText?.text.toString()
-
-
-            /* se o login_email e login_senha estiver vazios n deixa passar */
-            if (emailLogar1.isEmpty() || senhaLogar1.isEmpty()) {
-
-                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
-
+            } else if (binding.loginSenha.text.toString().isEmpty()) {
+                Toast.makeText(this, "Insira uma senha", Toast.LENGTH_SHORT).show()
             } else {
-
-                /* variaveis recebidas do cadastro */
-                val emailCadastrado = intent.getStringExtra("email")
-                val senhaCadastrada = intent.getStringExtra("senha")
-
-                if (emailLogar1 == emailCadastrado && senhaLogar1 == senhaCadastrada) {
-
-                    bntLogar.setOnClickListener {
-
+                auth.signInWithEmailAndPassword(
+                    binding.loginEmail.text.toString(),
+                    binding.loginSenha.text.toString()
+                ).addOnCompleteListener { result ->
+                    if (result.isSuccessful) {
                         val intentVagas = Intent(this, TelaInicial::class.java)
                         startActivity(intentVagas)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Não foi possível autenticar", Toast.LENGTH_SHORT)
+                            .show()
                     }
-
-                } else {
-
-                    Toast.makeText(this, "Email ou senha incorretos", Toast.LENGTH_SHORT).show()
                 }
+
             }
+
         }
 
-        /* intent para ir para o cadastro */
-        btnCadastrar.setOnClickListener{
+        binding.btnCadastrar.setOnClickListener(){
             val intentCadastrar = Intent(this, Cadastro::class.java)
             startActivity(intentCadastrar)
         }
 
     }
+
 }
